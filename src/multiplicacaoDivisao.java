0f4986d2f650f070a5bacb952814701d410ddff2
WillianBonettizx <william_bonetti@estudante.sesisenai.org.br>
2025-08-27 14:48:35 -0300

    Estrutura do projeto

--- src/multiplicacaoDivisao.java
+++
@@ -1,8 +0,0 @@
-public static int multiplicacao(int a, int b) {
-    return a * b;
-}
-
-public static double divisao(int a, int b) {
-    if (b == 0) throw new IllegalArgumentException("Divisão por zero não permitida!");
-    return (double) a / b;
-}
--- src/somaSubtracao.java
+++
@@ -1,8 +0,0 @@
-public class Calculadora {
-    public static int soma(int a, int b) {
-        return a + b;
-    }
-    public static int subtracao(int a, int b) {
-        return a - b;
-    }
-}
